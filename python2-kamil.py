cf8625ea97f6fa332222bd19b395045b6dc9fab6
MuhamadInsankamil <111480496+MuhamadInsankamil@users.noreply.github.com>
2022-08-18 00:11:26 -0700

    Add files via upload

---
+++ python2-kamil.py
@@ -0,0 +1,8 @@
+nama_depan="muhamad"
+nama_tengah="insan"
+nama_belakang="kamil"
+nama_lengkap="muhamad insan kamil"
+print("nama depan anda adalah",nama_depan)
+print("nama tengah saya adalah",nama_tengah)
+print("nama belakang saya adalah",nama_belakang)
+print("nama lengkap saya adalah",nama_depan,nama_tengah,nama_belakang)
