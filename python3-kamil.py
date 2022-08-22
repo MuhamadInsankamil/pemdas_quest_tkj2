db3c0fde32840826bac4e82f2fb10de28a2ef646
MuhamadInsankamil <111480496+MuhamadInsankamil@users.noreply.github.com>
2022-08-21 22:04:49 -0700

    Add files via upload

---
+++ python3-kamil.py
@@ -0,0 +1,19 @@
+nama_depan=input("masukan nama depan:")
+nama_tengah=input("masukan nama tengah:")
+nama_belakang=input("masukan nama belakang:")
+print("nama lengkap saya adalah",nama_depan,nama_tengah,nama_belakang)
+
+a=int(input("masukan nilai a:"))
+b=float(input("masukan nilai b:"))
+c=a + b
+
+print("A =", a)
+print("B =", b)
+print(a,"+",b,"=",c)
+
+
+#lengkapi scrit berikut ini
+
+x =int(input("Masukan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius=",y,  "fahreheit")
